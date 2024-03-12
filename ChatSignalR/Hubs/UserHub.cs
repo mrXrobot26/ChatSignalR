ecb0b0b7c7b408614ba4b88dcb0781fd009e5046
Abdo Elbessa <zezoelbessa10@egyeagles.com>
2024-03-12 23:40:42 +0200

    Add project files.

---
+++ ChatSignalR/Hubs/UserHub.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace ChatSignalR.Hubs
+{
+    public class UserHub :Hub
+    {
+        public int TotalViews { get; set; } = 0;
+        public async Task NewWindowLoaded()
+        {
+            TotalViews++;
+
+            await Clients.All.SendAsync("updateTotalViews", TotalViews);
+
+        }
+
+    }
+}
---
+++ ChatSignalR/wwwroot/js/UserCount.js
@@ -0,0 +1,23 @@
+﻿//create connetction
+var connectioToUserHub = new signalR.HubConnectionBuilder().withUri("/hubs/UserCount");
+//connect to method that hub invoke
+connectioToUserHub.on("updateTotalViews", (value) => {
+    var newCountSpan = document.getElementById("totalViewsCounter");
+    newCountSpan.innerText = value.toString();
+
+})
+//invoke hub methods aka send notification to hub
+function newWindowLoadedOnClient() {
+    connectionUserCount.send("NewWindowLoaded");
+}
+
+//start connection
+function fulfilled() {
+    //do something on start
+    console.log("Connection to User Hub Successful");
+}
+function rejected() {
+    //rejected logs
+}
+
+connectionUserCount.start().then(fulfilled, rejected);
